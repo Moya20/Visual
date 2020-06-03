d013f12d8921f19cbc0570f1a828576de8c24cde
Juan Andres <59864018+Moya20@users.noreply.github.com>
2020-06-03 12:03:55 +0200

    Creacion del proyecto

---
+++ ConsoleAppPruebaa/ConsoleAppPruebaa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppPruebaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
